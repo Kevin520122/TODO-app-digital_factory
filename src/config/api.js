a4aaaf877343d190eef4d4c2fc113b159360109c
Kevin520122 <zhaoshilong4@gmail.com>
2025-03-31 19:14:17 -0700

    rewrite env setting

--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,12 @@
-export const API_URL = process.env.TODO_APP_API_URL || 'http://localhost:5000';
+// console.log("All env variables:", process.env);
+// console.log("API URL:", process.env.REACT_APP_API_URL);
+// export const API_URL = process.env.REACT_APP_API_URL;
+const dev = {
+    API: "http://localhost:5000"
+  };
+  
+  const prod = {
+    API: "https://todo-app-backend-deploy-version.vercel.app"
+  };
+  
+  export const API_URL = process.env.NODE_ENV === 'production' ? prod.API : dev.API;
